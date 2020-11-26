755b2f57571e77bc38d11edc91634266493554df
huji3156@gmail.com <huji3156@gmail.com>
2020-11-27 01:41:33 +0900

    회원가입에 접근할 때는 인증이 필요없게 하기

--- server/movie_app/accounts/views.py
+++ server/movie_app/accounts/views.py
@@ -1,10 +1,12 @@
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from .serializers import UserSerializer
 
 
 @api_view(['POST'])
+@permission_classes([AllowAny])
 def signup(request):
     password = request.data.get('password')
     password_confirmation = request.data.get('passwordConfirmation')
